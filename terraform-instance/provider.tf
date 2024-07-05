b91a49657025712dc6f5428ecc208c25c48b8912
patrizio lattanzio <patrizio.lattanzio@consulenti.mediolanum.it>
2024-07-05 15:06:11 +0200

    change tf state

--- terraform-instance/provider.tf
+++ terraform-instance/provider.tf
@@ -13,3 +13,18 @@ provider "proxmox" {
   pm_api_token_secret = var.proxmox_api_token_secret
   pm_tls_insecure     = var.pm_tls_insecure
 }
+
+provider "aws" {
+  access_key                  = var.access_key
+  secret_key                  = var.secret_key_minio
+  region                      = "us-east-1" # Questo valore è ignorato ma necessario da Terraform
+  skip_requesting_account_id  = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  s3_use_path_style           = true
+
+  endpoints {
+    s3 = var.minio_url
+  }
+
+}
